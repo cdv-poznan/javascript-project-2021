a3d9d1d4b692418e6dbf714b492ef3933261d08d
DominikaDMT <dominikadmt@gmail.com>
2021-04-06 14:47:54 +0200

    update index.html and add ingredients aside

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('JavaScript Project');
+// BEM
+
+import './components/ingredientsAside';
+
+const navLinks = [
+  { button: 'ingsBtn', aside: 'ingredients' },
+  { button: 'menuBtn', aside: 'menu' },
+  { button: 'favsBtn', aside: 'favourites' },
+];
+
+for (const link of navLinks) {
+  $(`.${link.button}`).click(() => {
+    $('aside').css('display', 'none');
+    $(`.${link.aside}`).css('display', 'block');
+  });
+}
+
+$(document).ready(function () {
+  $('.ingredients').css('display', 'block');
+});
