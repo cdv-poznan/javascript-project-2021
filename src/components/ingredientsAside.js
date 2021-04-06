233cff6ea0c817f6015d0d0184943c66c8ae0f5e
DominikaDMT <dominikadmt@gmail.com>
2021-04-06 14:45:56 +0200

    create ingredientsAside.js

---
+++ src/components/ingredientsAside.js
@@ -0,0 +1,54 @@
+const getIngredients = () => {
+  const localStorageData = localStorage.getItem('ingredients');
+
+  let ingredients;
+  if (localStorageData) {
+    ingredients = localStorageData.split(',');
+  } else {
+    ingredients = [];
+  }
+  return ingredients;
+};
+
+const showIngredients = ingredients => {
+  ingredients.map(ing => {
+    return $('ul.ingredientsList').append(`<li>${ing}</li>`);
+  });
+};
+
+const addNewIngredient = () => {
+  const ingredients = getIngredients();
+
+  const newIngredient = $('input').val();
+  $('input').val('');
+  ingredients.push(newIngredient);
+  showIngredients([newIngredient]);
+  const ingredientsList = ingredients.join(',');
+  localStorage.setItem('ingredients', ingredientsList);
+};
+
+const deleteIngredient = e => {
+  const ingredients = getIngredients();
+  const newIngredientsList = ingredients.filter(
+    value => value !== e.target.innerText,
+  );
+  localStorage.setItem('ingredients', newIngredientsList);
+  e.target.closest('li').remove();
+};
+
+$('.addBtn').click(() => {
+  addNewIngredient();
+});
+
+$('input').keypress(e => {
+  const keycode = e.keyCode ? e.keyCode : e.which;
+  if (keycode === 13) {
+    addNewIngredient();
+  }
+});
+
+$('ul.ingredientsList').on('click', 'li', deleteIngredient);
+
+$(document).ready(function () {
+  showIngredients(getIngredients());
+});
