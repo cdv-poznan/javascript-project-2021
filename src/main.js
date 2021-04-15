ab152c761ec127bdebe3423a8cf6e7f5fe03b6e0
BorkowskiMichalCo <michal.borkowski.co@gmail.com>
2021-04-15 14:14:20 +0200

    add navigation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log('JavaScript Project');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelector('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
