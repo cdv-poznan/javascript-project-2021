d52051c222e630b9d86a60dcb57b8d4af217ab49
Marcin Ciesielski <emsique@gmail.com>
2021-04-19 13:47:59 +0200

    DOMContentLoaded fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   function setViewportHeight() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty(--vh, `${vh}px`);
