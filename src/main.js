81398676ca8a7c0d5cce81c4bc0f65b8c63f04e0
ddomarecki <damian.domarecki@gmail.com>
2021-04-22 09:47:16 +0200

    cards: the same and different functions added

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ function game() {
         ranNums = [],
         i = nums.length,
         j = 0;
-        addClass = [];
+    addClass = [];
 
     while (i--) {
         j = Math.floor(Math.random() * (i + 1));
@@ -18,6 +18,14 @@ function game() {
     //zmienne
     cardCompare = [];
     //funkcje
+    function different() {
+        // document.querySelector(".board div:nth-child" + "(" + addClass[0] + ")").classList.remove("card1")
+        document.querySelector(".board div:nth-child" + "(" + addClass[0] + ")").classList.remove("card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23", "card24")
+            
+        document.querySelector(".board div:nth-child" + "(" + addClass[1] + ")").classList.remove("card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18", "card19", "card20", "card21", "card22", "card23", "card24")
+
+    }
+
     // porównanie kart
     function sameCards(var1) {
         addClass.push(var1);
@@ -26,19 +34,31 @@ function game() {
             if (cardCompare[0] == cardCompare[1]) {
                 console.log("Te same!");
 
+                document.querySelector(".board div:nth-child" + "(" + addClass[0] + ")").classList.add("match");
+                document.querySelector(".board div:nth-child" + "(" + addClass[1] + ")").classList.add("match");
+
                 cardCompare = [];
+                addClass = [];
             } else {
                 console.log("Różne!");
-                var getCard2 = "getCard" + addClass[0];
-                // getCard.classList.add("card" + ranNums[0])        //problem tu występuje
-                getCard.classList.remove("card" + ranNums[0]);
+                // document.querySelector(".board div:nth-child" + "(" + addClass[0] + ")").addEventListener("click", function () {
+                //     console.log("TRACERT")
+                //     document.querySelector(".board div:nth-child" + "(" + addClass[0] + ")").classList.remove("card1");
+
+                //     document.querySelector(".board div:nth-child" + "(" + addClass[1] + ")").classList.remove("card1");
+                // });
+
+                different();
+                
+                // document.querySelector(".board div:nth-child" + "(" + addClass[1] + ")").classList.remove("card1");
+
                 cardCompare = [];
                 addClass = [];
             }
         }
     }
 
-    
+
     //karta 1
     // setInterval(function(){alert("Hello")},3000);
 
@@ -46,13 +66,11 @@ function game() {
         getCard1.classList.add("card" + ranNums[0]);
         cardCompare.push(ranNums[0]);
         console.log(cardCompare);
-        sameCards("1");  //dodać argument do funkcji i przekazać go do niej
+        sameCards("1"); //dodać argument do funkcji i przekazać go do niej
     };
     var getCard1 = document.querySelector(".board div:nth-child(1)");
-    getCard1.addEventListener("click", function (event) {
+    getCard1.addEventListener("click", function () {
         showCard();
-    }, {
-        once: true
     });
 
 
@@ -66,10 +84,8 @@ function game() {
     };
 
     var getCard2 = document.querySelector(".board div:nth-child(2)");
-    getCard2.addEventListener("click", function (event) {
+    getCard2.addEventListener("click", function () {
         showCard2();
-    }, {
-        once: true
     });
 
     //karta 3
@@ -78,13 +94,11 @@ function game() {
         getCard3.classList.add("card" + ranNums[2]);
         cardCompare.push(ranNums[2]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("3");
     }
     var getCard3 = document.querySelector(".board div:nth-child(3)");
-    getCard3.addEventListener("click", function (event) {
+    getCard3.addEventListener("click", function () {
         showCard3();
-    }, {
-        once: true
     });
 
     //karta 4
@@ -93,13 +107,11 @@ function game() {
         getCard4.classList.add("card" + ranNums[3]);
         cardCompare.push(ranNums[3]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("4");
     }
     var getCard4 = document.querySelector(".board div:nth-child(4)");
-    getCard4.addEventListener("click", function (event) {
+    getCard4.addEventListener("click", function () {
         showCard4();
-    }, {
-        once: true
     });
 
     //karta 5
@@ -108,13 +120,11 @@ function game() {
         getCard5.classList.add("card" + ranNums[4]);
         cardCompare.push(ranNums[4]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("5");
     }
     var getCard5 = document.querySelector(".board div:nth-child(5)");
-    getCard5.addEventListener("click", function (event) {
+    getCard5.addEventListener("click", function () {
         showCard5();
-    }, {
-        once: true
     });
 
 
@@ -124,13 +134,11 @@ function game() {
         getCard6.classList.add("card" + ranNums[5]);
         cardCompare.push(ranNums[5]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("6");
     }
     var getCard6 = document.querySelector(".board div:nth-child(6)");
-    getCard6.addEventListener("click", function (event) {
+    getCard6.addEventListener("click", function () {
         showCard6();
-    }, {
-        once: true
     });
 
 
@@ -140,13 +148,11 @@ function game() {
         getCard7.classList.add("card" + ranNums[6]);
         cardCompare.push(ranNums[6]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("7");
     }
     var getCard7 = document.querySelector(".board div:nth-child(7)");
-    getCard7.addEventListener("click", function (event) {
+    getCard7.addEventListener("click", function () {
         showCard7();
-    }, {
-        once: true
     });
 
 
@@ -156,13 +162,11 @@ function game() {
         getCard8.classList.add("card" + ranNums[7]);
         cardCompare.push(ranNums[7]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("8");
     }
     var getCard8 = document.querySelector(".board div:nth-child(8)");
-    getCard8.addEventListener("click", function (event) {
+    getCard8.addEventListener("click", function () {
         showCard8();
-    }, {
-        once: true
     });
 
 
@@ -172,13 +176,11 @@ function game() {
         getCard9.classList.add("card" + ranNums[8]);
         cardCompare.push(ranNums[8]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("9");
     }
     var getCard9 = document.querySelector(".board div:nth-child(9)");
-    getCard9.addEventListener("click", function (event) {
+    getCard9.addEventListener("click", function () {
         showCard9();
-    }, {
-        once: true
     });
 
     //karta 10
@@ -187,13 +189,11 @@ function game() {
         getCard10.classList.add("card" + ranNums[9]);
         cardCompare.push(ranNums[9]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("10");
     }
     var getCard10 = document.querySelector(".board div:nth-child(10)");
-    getCard10.addEventListener("click", function (event) {
+    getCard10.addEventListener("click", function () {
         showCard10();
-    }, {
-        once: true
     });
 
 
@@ -203,13 +203,11 @@ function game() {
         getCard11.classList.add("card" + ranNums[10]);
         cardCompare.push(ranNums[10]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("11");
     }
     var getCard11 = document.querySelector(".board div:nth-child(11)");
-    getCard11.addEventListener("click", function (event) {
+    getCard11.addEventListener("click", function () {
         showCard11();
-    }, {
-        once: true
     });
 
     //karta 12
@@ -218,13 +216,11 @@ function game() {
         getCard12.classList.add("card" + ranNums[11]);
         cardCompare.push(ranNums[11]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("12");
     }
     var getCard12 = document.querySelector(".board div:nth-child(12)");
-    getCard12.addEventListener("click", function (event) {
+    getCard12.addEventListener("click", function () {
         showCard12();
-    }, {
-        once: true
     });
 
     //karta 13
@@ -233,13 +229,11 @@ function game() {
         getCard13.classList.add("card" + ranNums[12]);
         cardCompare.push(ranNums[12]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("13");
     }
     var getCard13 = document.querySelector(".board div:nth-child(13)");
-    getCard13.addEventListener("click", function (event) {
+    getCard13.addEventListener("click", function () {
         showCard13();
-    }, {
-        once: true
     });
 
 
@@ -249,13 +243,11 @@ function game() {
         getCard14.classList.add("card" + ranNums[13]);
         cardCompare.push(ranNums[13]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("14");
     }
     var getCard14 = document.querySelector(".board div:nth-child(14)");
-    getCard14.addEventListener("click", function (event) {
+    getCard14.addEventListener("click", function () {
         showCard14();
-    }, {
-        once: true
     });
 
 
@@ -265,13 +257,11 @@ function game() {
         getCard15.classList.add("card" + ranNums[14]);
         cardCompare.push(ranNums[14]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("15");
     }
     var getCard15 = document.querySelector(".board div:nth-child(15)");
-    getCard15.addEventListener("click", function (event) {
+    getCard15.addEventListener("click", function () {
         showCard15();
-    }, {
-        once: true
     });
 
 
@@ -281,13 +271,11 @@ function game() {
         getCard16.classList.add("card" + ranNums[15]);
         cardCompare.push(ranNums[15]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("16");
     }
     var getCard16 = document.querySelector(".board div:nth-child(16)");
-    getCard16.addEventListener("click", function (event) {
+    getCard16.addEventListener("click", function () {
         showCard16();
-    }, {
-        once: true
     });
 
     //karta 17
@@ -296,13 +284,11 @@ function game() {
         getCard17.classList.add("card" + ranNums[16]);
         cardCompare.push(ranNums[16]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("17");
     }
     var getCard17 = document.querySelector(".board div:nth-child(17)");
-    getCard17.addEventListener("click", function (event) {
+    getCard17.addEventListener("click", function () {
         showCard17();
-    }, {
-        once: true
     });
 
     //karta 18
@@ -311,13 +297,11 @@ function game() {
         getCard18.classList.add("card" + ranNums[17]);
         cardCompare.push(ranNums[17]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("18");
     }
     var getCard18 = document.querySelector(".board div:nth-child(18)");
-    getCard18.addEventListener("click", function (event) {
+    getCard18.addEventListener("click", function () {
         showCard18();
-    }, {
-        once: true
     });
 
     //karta 19
@@ -326,13 +310,11 @@ function game() {
         getCard19.classList.add("card" + ranNums[18]);
         cardCompare.push(ranNums[18]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("19");
     }
     var getCard19 = document.querySelector(".board div:nth-child(19)");
-    getCard19.addEventListener("click", function (event) {
+    getCard19.addEventListener("click", function () {
         showCard19();
-    }, {
-        once: true
     });
 
     //karta 20
@@ -341,13 +323,11 @@ function game() {
         getCard20.classList.add("card" + ranNums[19]);
         cardCompare.push(ranNums[19]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("20");
     }
     var getCard20 = document.querySelector(".board div:nth-child(20)");
-    getCard20.addEventListener("click", function (event) {
+    getCard20.addEventListener("click", function () {
         showCard20();
-    }, {
-        once: true
     });
 
 
@@ -357,13 +337,11 @@ function game() {
         getCard21.classList.add("card" + ranNums[20]);
         cardCompare.push(ranNums[20]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("21");
     }
     var getCard21 = document.querySelector(".board div:nth-child(21)");
-    getCard21.addEventListener("click", function (event) {
+    getCard21.addEventListener("click", function () {
         showCard21();
-    }, {
-        once: true
     });
 
 
@@ -373,13 +351,11 @@ function game() {
         getCard22.classList.add("card" + ranNums[21]);
         cardCompare.push(ranNums[21]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("22");
     }
     var getCard22 = document.querySelector(".board div:nth-child(22)");
-    getCard22.addEventListener("click", function (event) {
+    getCard22.addEventListener("click", function () {
         showCard22();
-    }, {
-        once: true
     });
 
     //karta 23
@@ -388,13 +364,11 @@ function game() {
         getCard23.classList.add("card" + ranNums[22]);
         cardCompare.push(ranNums[22]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("23");
     }
     var getCard23 = document.querySelector(".board div:nth-child(23)");
-    getCard23.addEventListener("click", function (event) {
+    getCard23.addEventListener("click", function () {
         showCard23();
-    }, {
-        once: true
     });
 
     //karta 24
@@ -403,13 +377,11 @@ function game() {
         getCard24.classList.add("card" + ranNums[23]);
         cardCompare.push(ranNums[23]);
         console.log(cardCompare);
-        // sameCards();
+        sameCards("24");
     }
     var getCard24 = document.querySelector(".board div:nth-child(24)");
-    getCard24.addEventListener("click", function (event) {
+    getCard24.addEventListener("click", function () {
         showCard24();
-    }, {
-        once: true
     });
 
 
