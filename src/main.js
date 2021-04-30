3479d62c9ada73a051b9046900de1244ebf21d10
oc <oskar.cejman@nxo.pl>
2021-04-30 08:42:05 +0200

    still trying to fix webpack bug

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ const showJoke = document.querySelector('.showJoke')
 const showPunchline = document.querySelector('.showPunchline')
 const joke = document.querySelector('.joke')
 const puncH1 = document.querySelector('.puncH1')
-showJoke.addEventListener('click', function(){
-    axios.get('https://official-joke-api.appspot.com/jokes/random').then((response) => {
-        haha = response.data;
-        joke.innerText = haha.setup;
-        jokeFunction()
-})})
+function punchlineFunction(){
+    setTimeout(function(){
+        joke.innerHTML = '';
+    },5000)
+}
 function jokeFunction(){
     setTimeout(() => {
         audio.play();
@@ -19,50 +18,33 @@ function jokeFunction(){
         punchlineFunction()
     }, 5000);
 } 
-function punchlineFunction(){
-    setTimeout(function(){
-        joke.innerHTML = '';
-    },5000)
-}
+showJoke.addEventListener('click', function(){
+    axios.get('https://official-joke-api.appspot.com/jokes/random').then((response) => {
+        haha = response.data;
+        joke.innerText = haha.setup;
+        jokeFunction()
+})})
+
+
 // ---------------------------------------------------------------------------------------------------------------
 window.addEventListener("load", getItem);
 let inputValue = ' ';
-let buttonValue = document.querySelector('.buttonItem')
-let buttonClean = document.querySelector('.cleanButton')
+const buttonValue = document.querySelector('.buttonItem')
+const buttonClean = document.querySelector('.cleanButton')
 
 buttonValue.addEventListener('click',function(){
 inputValue = document.querySelector('.addItem').value;
 document.querySelector('.addItem').value = '';
-if(inputValue === ''){
-    $("#myModal").modal('show');
-// alert('Musisz coś wpisać, nie da sie niczego nie mieć do roboty ;)')
-}
-else{
-    console.log('Przeszło pierwszego ifa')
-if(localStorage.getItem('data') === null){
-    localStorage.setItem('data', '[]')
-}
-    old_data = JSON.parse(localStorage.getItem('data'))
-    old_data.push(inputValue)
-    localStorage.setItem('data', JSON.stringify(old_data))
-    console.log(old_data)
-    document.querySelector('#tableBody').textContent = '';
-    getItem()
-    }
-    
-}
-)
-
 function getItem(){
     if(localStorage.getItem('data')!== null){
         get_data = JSON.parse(localStorage.getItem('data'))
         console.log('zaladowalo z LS')
         for(let i = 0; i < get_data.length; i++){
         console.log(get_data[i])
-        let tr = document.createElement('tr');
-        let td = document.createElement('td');
-        let th = document.createElement('th');
-        let button = document.createElement('button')
+        const tr = document.createElement('tr');
+        const td = document.createElement('td');
+        const th = document.createElement('th');
+        const button = document.createElement('button')
         button.type = 'button'
         button.classList="btn-close"
         button.addEventListener('click', function(){
@@ -87,12 +69,33 @@ function getItem(){
        
     }
 }
-buttonClean.addEventListener('click', cleanAllContentFromLSAndList)
+if(inputValue === ''){
+    $("#myModal").modal('show');
+// alert('Musisz coś wpisać, nie da sie niczego nie mieć do roboty ;)')
+}
+else{
+    console.log('Przeszło pierwszego ifa')
+if(localStorage.getItem('data') === null){
+    localStorage.setItem('data', '[]')
+}
+    old_data = JSON.parse(localStorage.getItem('data'))
+    old_data.push(inputValue)
+    localStorage.setItem('data', JSON.stringify(old_data))
+    console.log(old_data)
+    document.querySelector('#tableBody').textContent = '';
+    getItem()
+    }
+    
+}
+)
+
 function cleanAllContentFromLSAndList(){
     localStorage.removeItem('data');
     document.querySelector('#tableBody').textContent = '';
 
 }
+buttonClean.addEventListener('click', cleanAllContentFromLSAndList)
+
 let google = ''
 
 
