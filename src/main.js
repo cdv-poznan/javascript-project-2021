f010feae91e188464b83eb03b23e3548dd749346
Ania Tyrchan <antyrchan@gmail.com>
2021-05-15 14:43:23 +0200

    main sample gallery

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-console.log('JavaScript Project');
+let cats = [];
+
+function init() {
+  fetch('./data.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      cats = data.cats;
+      const html = cats
+        .map(cat => {
+          return `
+      <div class="col-md-4">
+        <div class="card mb-3">
+            <img src="${cat.url}">
+            <div class="card-body" id="picture">
+                <p class="card-text" id="cats">Name: ${cat.name}</p>
+                <p class="card-text" id="cats">Colour: ${cat.colour}</p>
+                <p class="card-text" id="cats">Age: ${cat.age}</p>
+                <p class="card-text" id="cats">Description: ${cat.description}</p>
+            </div>
+        </div>
+      </div>
+      `;
+        })
+        .join('');
+      document
+        .querySelector('.allCats .row')
+        .insertAdjacentHTML('afterbegin', html);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+init();
