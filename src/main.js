e784bba3094864018a4e160212c19476a50d485a
BorkowskiMichalCo <michal.borkowski.co@gmail.com>
2021-05-19 14:32:21 +0200

    working show-recipe

--- src/main.js
+++ src/main.js
@@ -12,34 +12,72 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.querySelector('.recipe-close-btn');
 
 function getMealList() {
-  const searchInputText = document.getElementById('search-input').value.trim();
+  const searchInputTxt = document.getElementById('search-input').value.trim();
   fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`,
+    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`,
   )
     .then(response => response.json())
     .then(data => {
-      let recipe = '';
+      let html = '';
       if (data.meals) {
         data.meals.forEach(meal => {
-          recipe += `
-              <div class="meal-item" data-id - '${meal.idMeal}'>
-              <div class="meal-img">
-                <img src="${meal.strMealThumb}" alt="" />
+          html += `
+              <div class="meal-item" data-id ="${meal.idMeal}">
+                <div class="meal-img">
+                  <img src="${meal.strMealThumb}" alt="food" />
+                </div>
+                <div class="meal-name">
+                  <h3>${meal.strMeal}</h3>
+                  <a href="#" class="recipe-btn">Get Recipe</a>
+                </div>
               </div>
-              <div class="meal-name">
-                <h3>${meal.strMeal}</h3>
-                <a href="#" class="recipe-btn">Get Recipe</a>
-              </div>
-            </div>
               `;
         });
-        mealList.classList.remove('not-found');
+        mealList.classList.remove('notFound');
       } else {
-        recipe = 'Maybe something different?';
-        mealList.classList.add('not-found');
+        html = 'Sorry, what do you mean?';
+        mealList.classList.add('notFound');
       }
-      mealList.innerHTML = recipe;
+      mealList.innerHTML = html;
     });
 }
 
+function mealRecipeModal(meal) {
+  meal = meal[0];
+  const html = `
+    <div class="recipe-meal-img">
+      <img src="${meal.strMealThumb}" alt="" />
+    </div>
+    <h2 class="recipe-title">${meal.strMeal}</h2>
+    /* <p class="recipe-category">${meal.strCategory}</p> */
+    <div class="recipe-instruct">
+      <h3>Instructions:</h3>
+      <p>${meal.strInstructions}</p>
+      
+    </div>
+    
+    <div class="recipe-link">
+      <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+    </div>
+    `;
+  mealDetailsContent.innerHTML = html;
+  mealDetailsContent.parentElement.classList.add('show-recipe');
+}
+
+function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('recipe-btn')) {
+    const mealItem = e.target.parentElement.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`,
+    )
+      .then(response => response.json())
+      .then(data => mealRecipeModal(data.meals));
+  }
+}
+
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+  mealDetailsContent.parentElement.classList.remove('show-recipe');
+});
