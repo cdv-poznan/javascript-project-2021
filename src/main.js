98509600d3af9e2795aca54c026e3e5fff84cb99
Ania Tyrchan <antyrchan@gmail.com>
2021-05-23 23:26:28 +0200

    add new cat functionality

--- src/main.js
+++ src/main.js
@@ -1,10 +1,51 @@
+let actualURL = '';
+
+function fetchData() {
+  fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => {
+      if (!response.ok) {
+        throw Error('error');
+      }
+      return response.json();
+    })
+    .then(data => {
+      actualURL = data[0].url;
+      const html = `<image src=${data[0].url} class="">`;
+      document.querySelector('#catImage').innerHTML = html;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function findAnotherCat() {
+  console.log('findAnotherCat');
+
+  fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => {
+      if (!response.ok) {
+        throw Error('error');
+      }
+      return response.json();
+    })
+    .then(data => {
+      actualURL = data[0].url;
+      const html = `<image src=${data[0].url} class="">`;
+      document.querySelector('#catImage').innerHTML = html;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+fetchData();
+
 let cats = [];
 
 function init() {
   fetch('./data.json')
     .then(res => res.json())
     .then(data => {
-      console.log(data);
       cats = data.cats;
       const html = cats
         .map(cat => {
@@ -33,3 +74,72 @@ function init() {
 }
 
 init();
+
+document.querySelector('#saveBtn').addEventListener('click', () => {
+  if (!checkIfEmpty()) {
+    const newCat = {
+      name: document.getElementById('inputNewName').value,
+      age: document.getElementById('inputNewAge').value,
+      colour: document.getElementById('inputNewColour').value,
+      description: document.getElementById('inputNewDescription').value,
+      url: actualURL,
+    };
+
+    cats.push(newCat);
+
+    const html = `
+      <div class="col-md-4">
+        <div class="card mb-3">
+            <img src="${newCat.url}">
+            <div class="card-body" id="picture">
+                <p class="card-text" id="cats">Name: ${newCat.name}</p>
+                <p class="card-text" id="cats">Colour: ${newCat.colour}</p>
+                <p class="card-text" id="cats">Age: ${newCat.age}</p>
+                <p class="card-text" id="cats">Description: ${newCat.description}<p>
+            </div>
+        </div>
+      `;
+
+    document
+      .querySelector('.allCats .row')
+      .insertAdjacentHTML('afterbegin', html);
+
+    clearForm();
+    toggleView();
+  }
+});
+
+const allCatsView = document.querySelector('.allCats');
+const addNewCatView = document.querySelector('.addNewCat');
+
+const toggleView = () => {
+  allCatsView.style.display =
+    getComputedStyle(allCatsView).display === 'none' ? 'block' : 'none';
+  addNewCatView.style.display =
+    getComputedStyle(addNewCatView).display === 'none' ? 'block' : 'none';
+};
+
+const clearForm = () => {
+  document.getElementById('inputNewName').value = '';
+  document.getElementById('inputNewAge').value = '';
+  document.getElementById('inputNewColour').value = '';
+  document.getElementById('inputNewDescription').value = '';
+
+  findAnotherCat();
+};
+
+document.querySelector('#goToHome').addEventListener('click', () => {
+  clearForm();
+  toggleView();
+});
+document
+  .querySelector('#addNewCatBtn')
+  .addEventListener('click', () => toggleView());
+
+function checkIfEmpty() {
+  if (document.getElementById('inputNewName').value.trim() == '') {
+    document.getElementById('inputNewName').style.borderColor = 'red';
+    return true;
+  }
+  return false;
+}
