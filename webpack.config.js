0e74b305f1e5befaa230f180a180c04ae23d8ba5
n0ale <77386012+n0ale@users.noreply.github.com>
2021-05-31 14:21:40 +0200

    Add files via upload
    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,55 @@
-const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ESLintWebpackPlugin = require('eslint-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const configure = (env, args) => {
-  const PRODUCTION = args.mode === 'production';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-  return {
-    mode: args.mode || 'development',
-    entry: {
-      main: ['./src/main', './src/style.scss'],
-    },
-    output: {
-      path: resolve(__dirname, './dist'),
-      filename: '[name].js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: [
-            {
-              loader: 'babel-loader',
-            },
-          ],
-        },
-        {
-          test: /\.sc?ss$/,
-          use: [
-            env.WEBPACK_SERVE
-              ? {
-                  loader: 'style-loader',
-                }
-              : MiniCssExtractPlugin.loader,
-            { loader: 'css-loader' },
-            { loader: 'sass-loader' },
-          ],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.(svg|eot|woff|woff2|ttf)$/,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        favicon: './src/favicon.ico',
-        scriptLoading: 'defer',
-        meta: {
-          viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
-        },
-        minify: 'auto',
-      }),
-      new ESLintWebpackPlugin({
-        failOnError: false,
-        failOnWarning: false,
-        formatter: 'codeframe',
-      }),
-      new StylelintWebpackPlugin({
-        files: './src/*.scss',
-        failOnError: false,
-        failOnWarning: false,
-        formatter: 'string',
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: './src/assets',
-            to: 'assets',
-          },
-        ],
-        options: {
-          concurrency: 100,
-        },
-      }),
+const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    devServer: {
-      port: 4200,
-    },
-    performance: {
-      hints: false,
-    },
-    devtool: PRODUCTION ? 'source-map' : 'eval-source-map',
-  };
+  },
 };
 
-module.exports = configure;
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
