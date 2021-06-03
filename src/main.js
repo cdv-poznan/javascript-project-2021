8d3df686a243fc8e8758afdf1cc7145dda6525f1
magdalenasenger <magdalena.senger@br-ag.eu>
2021-06-03 10:55:58 +0200

    go up button added

--- src/main.js
+++ src/main.js
@@ -349,3 +349,22 @@ window.movieFilterFn = function() {
         }
     }
 
+//Get the button
+mybutton = document.querySelector(".go-up-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+// When the user clicks on the button, scroll to the top of the document
+window.topFunction = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
